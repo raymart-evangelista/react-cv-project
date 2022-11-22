be11e6389dc8b7edf58badb87ef0cf347dbf46a8
raymart-evangelista <raymart.s.evangelista@gmail.com>
2022-11-22 10:07:36 -0800

    turn Input class comp to func comp

--- src/components/Input.js
+++ src/components/Input.js
@@ -1,25 +1,17 @@
 import React, { Component } from "react";
-import uniqid from "uniqid"
 
-class Input extends Component {
-  constructor(props) {
-    super(props)
-  }
-
-  render() {
-    return (
-      <div>
-        <input
-          onChange={event => this.props.handleChildInput(event.target.value)}
-          type="text" 
-          name="" 
-          id={this.props.label} 
-          placeholder={this.props.labelText}
-          value={this.props.value}
-        />
-      </div>
-    )
-  }
+const Input = ({handleChildInput, label, labelText, value}) => {
+  return (
+    <div>
+      <input 
+        onChange={event => handleChildInput(event.target.value)}
+        type="text" 
+        id={label}
+        placeholder={labelText}
+        value={value}
+      />
+    </div>
+  )
 }
 
 export default Input
