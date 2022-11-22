766453db66f3b486d8151a271d7b4caf461a2318
raymart-evangelista <raymart.s.evangelista@gmail.com>
2022-11-22 10:12:52 -0800

    turn Submission class comp to func comp

--- src/components/Submission.js
+++ src/components/Submission.js
@@ -1,36 +1,25 @@
-import React, { Component } from "react"
-
-class Submission extends Component {
-  constructor(props) {
-    super(props)
-    this.generalInfo = this.props.generalInfo
-    this.education = this.props.education
-    this.experience = this.props.experience
-  }
-
-  render() {
-    return (
-      <div>
+const Submission = ({generalInfo, education, experience}) => {
+  return (
+    <div>
         <h1>General Information</h1>
-        <p>{this.generalInfo.name}</p>
-        <p>{this.generalInfo.email}</p>
-        <p>{this.generalInfo.phone}</p>
+        <p>{generalInfo.name}</p>
+        <p>{generalInfo.email}</p>
+        <p>{generalInfo.phone}</p>
 
         <h1>Education</h1>
-        <p>{this.education.school}</p>
-        <p>{this.education.study}</p>
-        <p>{this.education.startDate}</p>
-        <p>{this.education.endDate}</p>
+        <p>{education.school}</p>
+        <p>{education.study}</p>
+        <p>{education.startDate}</p>
+        <p>{education.endDate}</p>
 
         <h1>Work Experience</h1>
-        <p>{this.experience.company}</p>
-        <p>{this.experience.position}</p>
-        <p>{this.experience.tasks}</p>
-        <p>{this.experience.startDate}</p>
-        <p>{this.experience.endDate}</p>
-      </div>
-    )
-  }
+        <p>{experience.company}</p>
+        <p>{experience.position}</p>
+        <p>{experience.tasks}</p>
+        <p>{experience.startDate}</p>
+        <p>{experience.endDate}</p> 
+    </div>
+  )
 }
 
 export default Submission
